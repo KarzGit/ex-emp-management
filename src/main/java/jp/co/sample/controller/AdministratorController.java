124d03e24c0f94571b48c9efded95d11ce3dea8e
kazuakiaso <kazuaki.aso@rakus-partners.co.jp>
2022-02-07 16:00:29 +0900

    ログアウト機能実装完了

--- src/main/java/jp/co/sample/controller/AdministratorController.java
+++ src/main/java/jp/co/sample/controller/AdministratorController.java
@@ -66,4 +66,10 @@ public class AdministratorController {
 			return "forward:/employee/showList";
 		}
 	}
+	
+	@RequestMapping("/logout")
+	public String logout() {
+		session.invalidate();
+		return "redirect:/";
+	}
 }
